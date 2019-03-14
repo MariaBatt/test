1e55fbdce497a69b8a60dd91c1bfa2acd9718660
Maria Batt <mashabatt94@mail.ru>
2019-03-14 19:41:30 +0300

    changed base.html,index.html,language.html,languages.html,urls.py,views.pu,.gitignore

--- learning_foreign_languages/urls.py
+++ learning_foreign_languages/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'learning_foreign_languages'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    #Вывод всех языков.
+    path('languages/', views.languages, name='languages'),
+    # Страница с подробной информацией по языку
+    path('languages/<int:language_id>/', views.language, name='language')
 ]
--- learning_foreign_languages/views.py
+++ learning_foreign_languages/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
+from .models import Language
 
 # Create your views here.
 def index(request):
     """Домашняя страница приложения Learning Languages"""
     return render(request, 'learning_foreign_languages/index.html')
+
+
+def languages(request):
+    """Выводит список языков"""
+    languages = Language.objects.order_by('date_added')
+    context = {'languages': languages}
+    return render(request, 'learning_foreign_languages/languages.html', context)
+
+
+def language(request, language_id):
+    """Выводит один язык и все его записи"""
+    language = Language.objects.get(id=language_id)
+    vocabularies = language.vocabulary_set.order_by('-date_added')
+    context = {'languages': languages, 'vocabularies': vocabularies}
+    return render(request, 'learning_foreign_languages/languages.html', context)
