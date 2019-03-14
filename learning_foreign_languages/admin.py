a85e977b647cd5c4e5e53f776686d92c00a4057f
Maria Batt <mashabatt94@mail.ru>
2019-03-14 13:37:17 +0300

    changed admin.py

--- learning_foreign_languages/admin.py
+++ learning_foreign_languages/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from maria_bs.models import Language, Vocabulary
+from learning_foreign_languages.models import Language, Vocabulary
 
 admin.site.register(Language)
 admin.site.register(Vocabulary)
