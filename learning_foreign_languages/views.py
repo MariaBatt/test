afa6fbdc1a75963dd5f646bc260e8c69ffc378cb
Maria Batt <mashabatt94@mail.ru>
2019-03-14 13:20:52 +0300

    Created file urls.py and URL schema defined for learning_foreign_languages

--- learning_foreign_languages/views.py
+++ learning_foreign_languages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    """Домашняя страница приложения Learning Languages"""
+    return render(request, 'learning_foreign_languages/index.html')
